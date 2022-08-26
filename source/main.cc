36e753d1ebcee2d67093147f11dd5238e5fc53be
lanyi <lanyi@ra3.moe>
2022-08-26 05:14:55 +0200

    optimizations

--- source/main.cc
+++ source/main.cc
@@ -16,12 +16,15 @@ namespace a = boost::asio;
 namespace h = httplib;
 namespace j = nlohmann;
 namespace l = spdlog;
-namespace t = std::chrono;
 using namespace std::literals;
 using namespace a::experimental::awaitable_operators;
+using Tcp = a::ip::tcp;
+using Udp = a::ip::udp;
+using SteadyTimer = a::use_awaitable_t<>::as_default_on_t<a::steady_timer>;
+using UdpSocket = a::use_awaitable_t<>::as_default_on_t<Udp::socket>;
 
 a::awaitable<void> run_control_server();
-a::awaitable<void> process_control_server(a::ip::tcp::iostream connection);
+a::awaitable<void> process_control_server(Tcp::iostream connection);
 j::json get_status();
 a::awaitable<j::json> spawn_relay_server(j::json request);
 
@@ -29,8 +32,8 @@ struct Response
 {
     bool succeeded = false;
     std::string message;
-    a::ip::udp::endpoint endpoint_1;
-    a::ip::udp::endpoint endpoint_2;
+    Udp::endpoint endpoint_1;
+    Udp::endpoint endpoint_2;
 
     j::json to_json() const;
 };
@@ -49,14 +52,12 @@ private:
 public:
     static RelayServerNat& instance();
     a::ip::address_v4 public_ip() const;
-    void translate(a::ip::udp::endpoint& local_endpoint);
+    void translate(Udp::endpoint& local_endpoint);
 };
 
 class Connection
 {
 public:
-    using SteadyTimer = a::use_awaitable_t<>::as_default_on_t<a::steady_timer>;
-    using UdpSocket = a::use_awaitable_t<>::as_default_on_t<a::ip::udp::socket>;
     struct Formatter : fmt::formatter<std::string>
     {
         template <typename FormatContext>
@@ -69,41 +70,40 @@ private:
     std::uintmax_t m_id = ++s_counter;
     std::array<int, 2> m_debug_counters = { 3, 3 };
     bool m_cancelled = false;
-    t::steady_clock::time_point m_start_time = t::steady_clock::now(); // 记录连接的开始时间
-    t::steady_clock::time_point m_last_update; // 记录上次更新的时间（用来判断连接什么时候超时）
+    bool m_watchdog_alive_flag = true;
     std::array<std::array<std::byte, 2048>, 2> m_buffer = {}; // 用于收发数据的缓冲区
-    std::array<a::ip::udp::endpoint, 2> m_players; // 记录连接的两个玩家的地址
+    std::array<Udp::endpoint, 2> m_players; // 记录连接的两个玩家的地址
     std::array<UdpSocket, 2> m_sockets; // 与玩家连接的套接字
-    std::future<std::array<a::ip::udp::endpoint, 2>> m_server_local_endpoints; // 获取用于联通两个玩家的端口号
+    std::future<std::array<Udp::endpoint, 2>> m_server_local_endpoints; // 获取用于联通两个玩家的端口号
 
 public:
-    static a::awaitable<std::array<a::ip::udp::endpoint, 2>> start_relay
+    static a::awaitable<std::array<Udp::endpoint, 2>> start_relay
     (
-        std::array<a::ip::udp::endpoint, 2> players
+        std::array<Udp::endpoint, 2> players
     );
     static a::awaitable<void> run_relay
     (
         std::array<UdpSocket, 2> sockets,
-        std::array<a::ip::udp::endpoint, 2> players
+        std::array<Udp::endpoint, 2> players
     );
     Connection(std::array<UdpSocket, 2>&& sockets);
     ~Connection();
     a::awaitable<void> watchdog();
-    a::ip::udp::endpoint get_our_target(std::size_t index, a::ip::udp::endpoint&& their_target);
+    Udp::endpoint get_our_target(std::size_t index, Udp::endpoint their_target);
     a::awaitable<void> do_relay(std::size_t index);
 };
 
 struct EndPointFormatter : fmt::formatter<std::string>
 {
     template <typename FormatContext>
-    auto format(a::ip::udp::endpoint const& input, FormatContext& ctx) -> decltype(ctx.out());
+    auto format(Udp::endpoint const& input, FormatContext& ctx) -> decltype(ctx.out());
 };
 
 template<>
 struct fmt::formatter<Connection> : Connection::Formatter {};
 
 template<>
-struct fmt::formatter<a::ip::udp::endpoint> : EndPointFormatter {};
+struct fmt::formatter<Udp::endpoint> : EndPointFormatter {};
 
 int main()
 {
@@ -132,19 +132,19 @@ int main()
 
 a::awaitable<void> run_control_server()
 {
-    a::ip::tcp::endpoint port = { a::ip::address_v4::any(), 10086 };
-    a::ip::tcp::acceptor acceptor{ co_await a::this_coro::executor, port };
+    Tcp::endpoint port = { a::ip::address_v4::any(), 10086 };
+    Tcp::acceptor acceptor{ co_await a::this_coro::executor, port };
     while (true)
     {
         auto socket = co_await acceptor.async_accept(a::use_awaitable);
         a::co_spawn(co_await a::this_coro::executor, [&]
         {
-            return process_control_server(a::ip::tcp::iostream{ std::move(socket) });
+            return process_control_server(Tcp::iostream{ std::move(socket) });
         }, a::detached);
     }
 }
 
-a::awaitable<void> process_control_server(a::ip::tcp::iostream connection)
+a::awaitable<void> process_control_server(Tcp::iostream connection)
 {
     j::json response;
     try
@@ -200,7 +200,7 @@ a::awaitable<j::json> spawn_relay_server(j::json request)
             }
             text.resize(separator_position);
             auto ip = a::ip::address_v4::from_string(text);
-            return a::ip::udp::endpoint{ ip, static_cast<std::uint16_t>(port) };
+            return Udp::endpoint{ ip, static_cast<std::uint16_t>(port) };
         }
         catch (std::exception const& e)
         {
@@ -278,19 +278,19 @@ a::ip::address_v4 RelayServerNat::public_ip() const
     return m_public_ip;
 }
 
-void RelayServerNat::translate(a::ip::udp::endpoint& local_endpoint)
+void RelayServerNat::translate(Udp::endpoint& local_endpoint)
 {
     std::scoped_lock lock{ m_mutex };
     local_endpoint.address(m_public_ip);
 }
 
-a::awaitable<std::array<a::ip::udp::endpoint, 2>> Connection::start_relay
+a::awaitable<std::array<Udp::endpoint, 2>> Connection::start_relay
 (
-    std::array<a::ip::udp::endpoint, 2> players
+    std::array<Udp::endpoint, 2> players
 )
 {
     auto strand = a::make_strand(co_await a::this_coro::executor);
-    a::ip::udp::endpoint any_port = { a::ip::address_v4::any(), 0 };
+    Udp::endpoint any_port = { a::ip::address_v4::any(), 0 };
     std::array sockets = { UdpSocket{ strand, any_port }, UdpSocket{ strand, any_port } };
     std::array local_endpoints = { sockets[0].local_endpoint(), sockets[1].local_endpoint() };
     a::co_spawn(strand, [sockets = std::move(sockets), players]() mutable
@@ -303,14 +303,13 @@ a::awaitable<std::array<a::ip::udp::endpoint, 2>> Connection::start_relay
 a::awaitable<void> Connection::run_relay
 (
     std::array<UdpSocket, 2> sockets,
-    std::array<a::ip::udp::endpoint, 2> players
+    std::array<Udp::endpoint, 2> players
 )
 {
     l::info("Creating relay for {} and {}...", players[0], players[1]);
     try
     {
         auto connection = Connection{ std::move(sockets) };
-        connection.m_last_update = t::steady_clock::now();
         connection.m_players = { players[1], players[0] };
         l::info("{} serving for players {} and {}", connection, players[0], players[1]);
         co_await(connection.watchdog() and connection.do_relay(0) and connection.do_relay(1));
@@ -340,8 +339,7 @@ a::awaitable<void> Connection::watchdog()
     SteadyTimer timer{ co_await a::this_coro::executor };
     while (true)
     {
-        if ((t::steady_clock::now() - m_last_update > 1min)
-            or not s0.is_open() or not s1.is_open())
+        if (not m_watchdog_alive_flag or not s0.is_open() or not s1.is_open())
         {
             m_cancelled = true;
             l::info("{} is being cancelled by watchdog", *this);
@@ -354,16 +352,15 @@ a::awaitable<void> Connection::watchdog()
             }
             break;
         }
-        timer.expires_after(30s);
+        m_watchdog_alive_flag = false;
+        timer.expires_after(45s);
         co_await timer.async_wait();
     };
 }
 
-a::ip::udp::endpoint Connection::get_our_target(std::size_t index, a::ip::udp::endpoint&& their_target)
+Udp::endpoint Connection::get_our_target(std::size_t index, Udp::endpoint their_target)
 {
-    auto now = t::steady_clock::now();
-    m_last_update = now;
-    m_players[std::size_t{ 1 } - index] = std::move(their_target);
+    m_players[std::size_t{ 1 } - index] = their_target;
     return m_players[index];
 }
 
@@ -372,14 +369,15 @@ a::awaitable<void> Connection::do_relay(std::size_t index)
     std::string name = fmt::format("{} {}<{}>", *this, std::source_location::current().function_name(), index);
     try
     {
-        auto& receiver = m_sockets[std::size_t{ 1 } - index];
-        auto& sender = m_sockets[index];
-        auto& buffer = m_buffer[index];
+        UdpSocket& receiver = m_sockets[std::size_t{ 1 } - index];
+        UdpSocket& sender = m_sockets[index];
+        std::array<std::byte, 2048>& buffer = m_buffer[index];
+        Udp::endpoint from;
         while (not m_cancelled)
         {
-            a::ip::udp::endpoint from;
             auto bytes_read = co_await receiver.async_receive_from(a::buffer(buffer), from);
-            auto to = get_our_target(index, std::move(from));
+            auto to = get_our_target(index, from);
+            m_watchdog_alive_flag = true;
             if (m_debug_counters[index] > 0)
             {
                 --m_debug_counters[index];
@@ -418,7 +416,7 @@ auto Connection::Formatter::format(Connection const& input, FormatContext& ctx) 
 }
 
 template <typename FormatContext>
-auto EndPointFormatter::format(a::ip::udp::endpoint const& input, FormatContext& ctx) -> decltype(ctx.out())
+auto EndPointFormatter::format(Udp::endpoint const& input, FormatContext& ctx) -> decltype(ctx.out())
 {
     return fmt::format_to(ctx.out(), "{}:{}", input.address().to_string(), input.port());
 }
